75a69da7299309d5da80996ca9a5c123496c9fff
brialln <brianallenlm913@gmail.com>
2024-10-23 18:40:28 +0800

    Updated ADMIN | Oct. 23, 2024

--- ADMIN/JS/app.js
+++ ADMIN/JS/app.js
@@ -30,26 +30,3 @@ function closeAllSubMenus(){
         ul.previousElementSibling.classList.remove('rotate')
     })
 }
-
-
-// Get the modal and button
-var modal = document.getElementById("uploadModal");
-var btn = document.getElementById("uploadBtn");
-var closeBtn = document.getElementsByClassName("close-pop")[0];
-
-// When the user clicks the button, open the modal
-btn.onclick = function() {
-    modal.style.display = "block";
-}
-
-// When the user clicks on the close button, close the modal
-closeBtn.onclick = function() {
-    modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
