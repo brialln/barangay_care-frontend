3fe459bc61c15d4c23bba926838094dfd6dec10e
brialln <brianallenlm913@gmail.com>
2024-10-24 21:27:30 +0800

    Updated RESIDENTS | Oct. 24, 2024

--- RESIDENTS/JS/app.js
+++ RESIDENTS/JS/app.js
@@ -1,4 +1,18 @@
-// WINDOW NAV SCROLL ANIMATION
+// GENERAL JS FUNCTION
+
+// Window Navigation Scroll Animation
 window.addEventListener('scroll', () => {
     document.querySelector('nav').classList.toggle('window-scroll', window.scrollY > 0)
 })
+
+// Page refresh once
+window.onload = function() {
+    // Check if the page was refreshed before
+    if (!sessionStorage.getItem('refreshed')) {
+      // If not, set a delay before refreshing the page
+      setTimeout(function() {
+        sessionStorage.setItem('refreshed', 'true');
+        location.reload();
+      }, 2500); // Refresh after 2500 milliseconds (2.5 seconds)
+    }
+  }
